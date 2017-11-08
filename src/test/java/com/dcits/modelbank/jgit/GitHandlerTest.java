d2ab27826d897a35f73a53c231d8de95798aebf7
JGIT <JGIT@dcits.com>
2017-11-08 15:06:16 +0800

    commitAndPush

--- src/test/java/com/dcits/modelbank/jgit/GitHandlerTest.java
+++ src/test/java/com/dcits/modelbank/jgit/GitHandlerTest.java
@@ -1,12 +1,14 @@
 package com.dcits.modelbank.jgit;
 
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -17,7 +19,7 @@ import java.util.List;
 public class GitHandlerTest {
     private ApplicationContext context;
     private GitHandler gitHandler;
-    
+
     @Before
     public void setUp() throws Exception {
         context = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -34,6 +36,70 @@ public class GitHandlerTest {
         List<String> files = new ArrayList<>();
         files.add("src/test/java/com/dcits/modelbank/jgit/GitHandlerTest.java");
         gitHandler.commitAndPush(files, "commitAndPush", true);
+    }
+
+    @Test
+    public void checkoutBranch() throws Exception {
+
+    }
+
+    @Test
+    public void checkoutNewBranch() throws Exception {
+    }
+
+    @Test
+    public void stash() throws Exception {
+        boolean flag = gitHandler.stash();
+        Assert.assertEquals(true, flag);
+    }
+
+    @Test
+    public void stashList() throws Exception {
+        Collection<RevCommit> stashes = gitHandler.stashList();
+        for (RevCommit revCommit : stashes) {
+            System.out.println(revCommit.getName());
+            System.out.println(revCommit);
+        }
+    }
+
+    @Test
+    public void unstash() throws Exception {
+    }
+
+    @Test
+    public void fetch() throws Exception {
+    }
+
+    @Test
+    public void pull() throws Exception {
+    }
+
+    @Test
+    public void pull1() throws Exception {
+    }
+
+    @Test
+    public void push() throws Exception {
+    }
+
+    @Test
+    public void commitAndPushAllChanges1() throws Exception {
+    }
+
+    @Test
+    public void commitAndPush1() throws Exception {
+    }
+
+    @Test
+    public void showBranchDiff() throws Exception {
+    }
+
+    @Test
+    public void showCommitDiff() throws Exception {
+    }
+
+    @Test
+    public void rollBackPreRevision1() throws Exception {
     }
 
     @Test
