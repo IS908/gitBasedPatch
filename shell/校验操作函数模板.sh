ad59ca4ef1dccaf4678a7ed2a5e76ee801294b3c
Kevin <kunchen@pku.edu.cn>
2017-11-27 15:59:58 +0800

    update

--- shell/校验操作函数模板.sh
+++ shell/校验操作函数模板.sh
@@ -6,7 +6,7 @@ APP_RUN_STATUS=-10
 MSG_START_SUCCESS='   \t    APP应用启动状态'
 MSG_STOP_SUCCESS='   \t    APP应用停止状态'
 MSG_STATUS_ERROR='   \t    APP应用状态未知,请人工确认当前状态'
-echo Galaxy App Pid：$PID_APP
+echo Galaxy App Pid: $PID_APP
 echo $APP_RUN_STATUS
 
 CheckFrontStat(){
