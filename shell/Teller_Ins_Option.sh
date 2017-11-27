90ea232f22967e023173777082344670a0117357
zhangji <zhangjig@dcits.com>
2017-11-27 21:12:05 +0800

    提交

--- shell/Teller_Ins_Option.sh
+++ shell/Teller_Ins_Option.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/.bashrc
+#source ~/.bashrc
 
 echo **********************************************************
 echo **                                                      **
@@ -18,18 +18,19 @@ echo **********************************************************
 #################### Var Setting START ####################
 FILE_PATH=`pwd`
 RUNDATE=`date +%Y%m%d`
-TAG_NO=01
+TAG_NO=02
 BUILD_PATH=${FILE_PATH}
 BUILD_PROPERTIES=${BUILD_PATH}/build.properties
 INCFILE=${FILE_PATH}/RUNALL/app_${RUNDATE}${TAG_NO}.txt
+ANT_HOME=${FILE_PATH}/tools\ant\
 
 MSG_NOT_EXIST_PROPERTIES='不存在增量执行文件build.properties，不可以进行打增量版本'
 MSG_NOT_EXIST_INCFILE='不存在增量清单文件，不可以进行打增量版本'
 strA="SmartTeller9\trans"
 strB=".jar"
-BUILD1=""
-BUILD2=""
 FIRST=0
+BUILD=""
+TEMP=""
 #################### Var Setting END ####################
 
 cd ${BUILD_PATH}
@@ -47,8 +48,6 @@ if [ ! -e "$INCFILE" ]; then
 fi
 
 # 读取增量描述文件，进行增量配置文件的处理
-##逐行读取增量清单文件，根据空格，制表符，换行符进行分割读取
-##将SmartTeller9\\trans下需要编译的进行编译
 for line in $(cat ${INCFILE})
 do 
     if [[ "$line" =~ "${strA}" ]]
@@ -58,12 +57,12 @@ do
         then
     #		echo "包含jar"
             if test ${FIRST} -ne 0;then
-                BUILD2=`echo ${BUILD1},${line}`
+                BUILD=`echo ${TEMP},${line}`
             else
-                BUILD2=`echo ${line}`
+                BUILD=`echo ${line}`
                 FIRST=1
             fi
-            BUILD1=${BUILD2}
+            TEMP=${BUILD}
 #        else
 #            echo "不包含jar"
         fi
@@ -72,10 +71,14 @@ do
     fi
 done
 
-echo $BUILD2 | `sed -i "s/SmartTeller9\trans\//g"`
+#筛选需要编译的交易
+BUILD=${BUILD//SmartTeller9\\trans\\/}
+BUILD=${BUILD//.jar/}
+echo "需要打版本的交易有："$BUILD
+sed -i "/sourceBase=/s/=.*/=${BUILD//\\/\\\\}/" build.properties
 
 # 进行增量交易的编译
-
+ant -buildfile build.xml
 # 进行增量抽取工作
 
 # 进行增量包的发布
