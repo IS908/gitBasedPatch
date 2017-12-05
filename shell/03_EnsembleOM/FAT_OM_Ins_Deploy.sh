37999312c08e7e063690ad0d0f417c0a94bf7368
zhangji <zhangjig@dcits.com>
2017-12-05 19:17:02 +0800

    提交

--- shell/03_EnsembleOM/FAT_OM_Ins_Deploy.sh
+++ shell/03_EnsembleOM/FAT_OM_Ins_Deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-source ~/.bashrc
+source ~/.bashr
 
+######## 增量抽取脚本 - begin ########
 echo **********************************************************
 echo **                                                      **
 echo **          EnsembleOM Ins Deploy Shell                 **
@@ -8,14 +9,24 @@ echo **              http://www.dcits.com                    **
 echo **            author:zhangjig@dcits.com                 **
 echo **                                                      **
 echo **********************************************************
-
+# 脚本说明：
+# 部署包备份，在部署的相应服务器上进行备份，其它位置不做备份，
+# 备份目录：
+# 全量包：~/backup/EnsembleOM/App_SmartEnsembleOM_${TAG_NO}.tar.gz
+# 增量包：~/backup/EnsembleOM/XXX.zip
+# 其中 ${TAG_NAME} 与 GitLab 上的 Tag 保持一致
+# 
+# 1、停止当前应用服务
+# 2、备份增量包到指定目录
+# 3、解压增量包并安装增量包
+# 4、启动服务
 
 ######## Var Setting START ########
 #run_status=`netstat -anp|grep 18889|awk '{printf $7}'|cut -d/ -f1`
 # 应用端口号，注意需加单引号
 PORT_APP='18889'
-# 启动应用检查时间间隔设定(单位：10秒)
-CHECK_TIME=9
+# 启动应用检查时间间隔设定(单位：秒)
+CHECK_TIME=30
 
 # 应用状态 APP_RUN_STATUS - 0：停止状态；1：启动状态
 APP_RUN_STATUS=-10
@@ -24,32 +35,39 @@ MSG_STOP_SUCCESS='APP应用停止状态'
 MSG_STOP_FAILD='APP应用停止失败，请人工停止原应用并部署'
 MSG_STATUS_ERROR='APP应用状态未知,请人工确认当前状态'
 
+###############################
+### 来自Jenkins的变量TAG_NO ###
+###############################
+
+APP_NAME=ModelBank
 DCITS_HOME=/app/dcits
-OM_HOME=${DCITS_HOME}
-BACKUP_HOME=${DCITS_HOME}/backup/EnsembleOM/
-TAR_GZ_HOME=${BACKUP_HOME}
+BACKUP_HOME=${DCITS_HOME}/backup/${APP_NAME}
+TAG_NAME=ModelBank_Ins_${TAG_NO}
+BACKUP_TEMP=${BACKUP_HOME}/${TAG_NAME}
+ZIP_HOME=${BACKUP_TEMP}/modules/modelBank-all-integration/target
 ######## Var Setting END ########
 
 ######## Function START ########
+# 检查应用当前状态
+CheckAppState() {
+    PID_APP=`/usr/sbin/lsof -n -P -t -i :${PORT_APP}`
+    echo 'PID_APP: ' ${PID_APP}
+    APP_RUN_STATUS=`ps -ef | grep ${PID_APP} | grep -v 'grep' | wc -l`
+    echo 'APP_RUN_STATUS: ' ${APP_RUN_STATUS}
+}
+
 # 检查应用是否停止 并返回状态码：停止成功:1；停止失败:0
 CheckStopState(){
-    OLD_PID_APP=`/usr/sbin/lsof -n -P -t -i :${PORT_APP}`
-	echo 'OLD_PID_APP:' $OLD_PID_APP
-    APP_RUN_STATUS=`ps -ef | grep $OLD_PID_APP | grep -v 'grep' | wc -l`
-	echo 'APP_RUN_STATUS:' $APP_RUN_STATUS
-
-    if [ $APP_RUN_STATUS -eq 0 ];then
+    CheckAppState
+    if [ ${APP_RUN_STATUS} -eq 0 ];then
         # 成功停止
-        echo $MSG_STOP_SUCCESS
+        echo ${MSG_STOP_SUCCESS}
     fi
 }
 
 # 检查应用是否启动 并返回状态码：启动成功:1；启动失败:0
 CheckStartState() {
-    PID_APP=`/usr/sbin/lsof -n -P -t -i :${PORT_APP}`
-    echo 'PID_APP:' $PID_APP
-    APP_RUN_STATUS=`ps -ef | grep ${PID_APP} | grep -v 'grep' | wc -l`
-    echo 'APP_RUN_STATUS:' ${APP_RUN_STATUS}
+    CheckAppState
     if [ ${APP_RUN_STATUS} -eq 1 ]
     then
         echo ${MSG_START_SUCCESS}
@@ -59,25 +77,43 @@ CheckStartState() {
     fi
 }
 
+# 间隔状态检查
 CHECK_INTERVAL() {
     for i in `seq $1`
     do
+        echo 'check' ${i}0s
         sleep 10s
-        echo 'check' ${i}
+        CheckAppState
+        if [ ${APP_RUN_STATUS} -ne 0 ];then
+            break
+        fi
     done
+}
+
+# 新应用发布成功后，备份被替换的旧应用（作为增量替换前的全量状态，以便增量发布后可回退上一个版本）
+BACKUP_OLD_APP() {
+    versionNum=`cat ${DCITS_HOME}/${APP_NAME}-old/versionid.txt`
+    cd ${DCITS_HOME}
+    tar -czf ${BACKUP_HOME}/${versionNum}-end.tar.gz ${APP_NAME}-old
+    rm -rf ${DCITS_HOME}/${APP_NAME}-old
+
+    # 部署成功，更新versionid.txt，并在versionlist.txt中追加增量版本号
+    echo App_${TAG_NAME} > ${DCITS_HOME}/${APP_NAME}/versionid.txt
+    echo App_${TAG_NAME} >> ${DCITS_HOME}/${APP_NAME}/version_list.txt
 }
 ######## Function END ########
 
-# 备份全量包，并解压包已备部署 DONE
-cd ${BACKUP_HOME}
-unzip ${BACKUP_HOME}/ensemble-om-1.0.4-SNAPSHOT-assembly.zip
-mv ${BACKUP_HOME}/ensemble-om-1.0.4-SNAPSHOT ${BACKUP_HOME}/ensemble-om
+# 移动增量包到相应备份目录下
+mv  ${ZIP_HOME}/app_modelbank_ins.zip  ${BACKUP_HOME}/App_${TAG_NAME}.zip
+cd ${BACKUP_TEMP}
+unzip ${BACKUP_HOME}/App_${TAG_NAME}.zip
+mv modelBank-integration ${APP_NAME}
 
 # 检查并停止应用，以备部署新应用
 CheckStopState
 if [ ${APP_RUN_STATUS} -ne 0 ];then
     echo 'App stopping ...'
-    sh ${OM_HOME}/ensemble-om/bin/stop.sh
+    sh ${DCITS_HOME}/${APP_NAME}/bin/stop.sh
 	CHECK_INTERVAL 1
     for i in `seq 3`
     do   
@@ -85,7 +121,7 @@ if [ ${APP_RUN_STATUS} -ne 0 ];then
         if [ ${APP_RUN_STATUS} -eq 0 ];then
             break
         fi
-        CHECK_INTERVAL 3
+        CHECK_INTERVAL 6
     done
     if [ ${APP_RUN_STATUS} -ne 0 ];then
         # 停止失败
@@ -95,40 +131,41 @@ if [ ${APP_RUN_STATUS} -ne 0 ];then
 fi
 
 # 备份原应用包
-cd ${OM_HOME}
-if [[ -d ${OM_HOME}/ensemble-om-old/ ]];then
-    rm -rf ${OM_HOME}/ensemble-om-old
+cd ${DCITS_HOME}
+if [[ -d ${DCITS_HOME}/${APP_NAME}-old/ ]];then
+    rm -rf ${DCITS_HOME}/${APP_NAME}-old
 fi
 
-if [[ -d ${OM_HOME}/ensemble-om/ ]];then
-    mv ${OM_HOME}/ensemble-om ${OM_HOME}/ensemble-om-old
+if [[ -d ${DCITS_HOME}/${APP_NAME}/ ]];then
+    cp -r ${DCITS_HOME}/${APP_NAME} ${DCITS_HOME}/${APP_NAME}-old
 fi
 
-# 部署新的应用包，并启动新应用
-mv ${BACKUP_HOME}/ensemble-om ${OM_HOME}
+# 部署增量应用包，并启动应用
+mv -f ${BACKUP_TEMP}/${APP_NAME}/lib/* ${DCITS_HOME}/${APP_NAME}/lib/
+rm -rf ${BACKUP_TEMP}
 echo 'App starting ...'
-sh ${OM_HOME}/ensemble-om/bin/start.sh
+sh ${DCITS_HOME}/${APP_NAME}/bin/start.sh
 CHECK_INTERVAL ${CHECK_TIME}
-
 
 # 检查新部署应用是否启动成功
 CheckStartState
 if [ ${APP_RUN_STATUS} -eq 1 ];then
-    # 新应用启动，删除旧应用
-    rm -rf ${OM_HOME}/ensemble-om-old
+    # 新应用启动，备份并删除旧应用
+    BACKUP_OLD_APP
     echo ${MSG_START_SUCCESS}
 else
     for i in `seq 5`
     do   
         CheckStartState
         if [ ${APP_RUN_STATUS} -eq 1 ];then
-            # 新应用启动，删除旧应用
-            rm -rf ${OM_HOME}/ensemble-om-old
+            # 新应用启动，备份并删除旧应用
+            BACKUP_OLD_APP
             echo ${MSG_START_SUCCESS}
             break
         else
+            sh ${DCITS_HOME}/${APP_NMAE}/bin/stop.sh
             echo 'Retry App starting ...'
-            sh ${OM_HOME}/ensemble-om/bin/start.sh
+            sh ${DCITS_HOME}/${APP_NAME}/bin/start.sh
         fi
         CHECK_INTERVAL ${CHECK_TIME}
     done
