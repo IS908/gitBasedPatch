b6b928117d5441c817262fbb1185a6a8ae7a1933
Kevin <kunchen@pku.edu.cn>
2017-12-18 15:50:37 +0800

    脚本更新

--- shell/OMS_SHELL/01_ModelBank/FAT_113_ModelBank_Incr.sh
+++ shell/OMS_SHELL/01_ModelBank/FAT_113_ModelBank_Incr.sh
@@ -7,21 +7,11 @@ FLAG_INCR=Incr
 
 hostIp=57.25.2.111
 moType=ModelBank
-versionNo=FAT_113_ModelBank_Ins_${TAG_NO}
+versionNo=FAT_113_ModelBank_Incr_${TAG_NO}
 fileType=Full
 fileName=${versionNo}.tar.gz
 OMS_HOME=/app/dcits/oms/jenkins_space
 TARGET=${WORKSPACE}/modules/modelBank-all-integration/target
-
-
-DEPLOY_FULL() {
-    cd ${TARGET}/modelBank-integration-assembly
-    rm -rf modelBank-integration
-    tar -zxf ${TARGET}/modelBank-integration-assembly.tar.gz
-    mv modelBank-integration ${versionNo}
-    tar -czf ${fileName} ${versionNo}
-    mv ${fileName} ${OMS_HOME}
-}
 
 DEPLOY_INCR() {
     cd ${TARGET}/PatchTmp
@@ -31,16 +21,7 @@ DEPLOY_INCR() {
 }
 
 # 进行全量增量判断
-if [[ "${BUILD_FLAG}" == "${FLAG_INCR}" ]]
-then
-    echo ${FLAG_INCR}
-    DEPLOY_INCR
-else if [[ "${BUILD_FLAG}" == "${FLAG_FULL}" ]]
-then
-    echo ${FLAG_FULL}
-    DEPLOY_FULL
-    fi
-fi
+DEPLOY_INCR
 
 # 向OMS发送通知
 RESULT=`curl -G -i http://57.25.2.137:9991/oms/jenkinsPostVersion?hostIp=${hostIp}\&moType=${moType}\&versionNo=${versionNo}\&fileType=${fileType}\&fileName=${fileName}`
