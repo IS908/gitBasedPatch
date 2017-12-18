8b1cd6fa9f4110585195033646e3eded55f6a9af
Kevin <kunchen@pku.edu.cn>
2017-12-18 15:49:25 +0800

    脚本更新

--- shell/OMS_SHELL/01_ModelBank/FAT_113_ModelBank_Full.sh
+++ shell/OMS_SHELL/01_ModelBank/FAT_113_ModelBank_Full.sh
@@ -23,24 +23,7 @@ DEPLOY_FULL() {
     mv ${fileName} ${OMS_HOME}
 }
 
-DEPLOY_INCR() {
-    cd ${TARGET}/PatchTmp
-    mv ModelBank ${versionNo}
-    tar -czf ${fileName} ${versionNo}
-    mv ${fileName} ${OMS_HOME}
-}
-
-# 进行全量增量判断
-if [[ "${BUILD_FLAG}" == "${FLAG_INCR}" ]]
-then
-    echo ${FLAG_INCR}
-    DEPLOY_INCR
-else if [[ "${BUILD_FLAG}" == "${FLAG_FULL}" ]]
-then
-    echo ${FLAG_FULL}
-    DEPLOY_FULL
-    fi
-fi
+DEPLOY_FULL
 
 # 向OMS发送通知
 RESULT=`curl -G -i http://57.25.2.137:9991/oms/jenkinsPostVersion?hostIp=${hostIp}\&moType=${moType}\&versionNo=${versionNo}\&fileType=${fileType}\&fileName=${fileName}`
